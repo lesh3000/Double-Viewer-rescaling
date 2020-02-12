e6e55310fc5991e194eb810fe5146f05d9357005
lesh3000 <30319422+lesh3000@users.noreply.github.com>
2020-02-12 11:43:50 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using GdPicture14;
+using GdPicture14.WEB;
+
+namespace aspnet_mvc_razor_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Version = DocuViewareManager.GetVersion();
+            return View();
+        }
+
+      
+    }
+}
